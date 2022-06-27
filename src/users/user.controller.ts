ac99e68e4d0724b9e1de9ae5b06c9e1c352c575e
Nguyen Cao Nhan <nhan.nguyencao@yoot.vn>
2022-06-28 01:05:51 +0700

    Avoid duplicate when create user, add find by firebase_id

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
@@ -7,14 +7,27 @@ import { UserService } from './user.service';
 @Controller('users')
 @ApiTags('User')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  logger: Logger;
+  constructor(private readonly userService: UserService) {
+    this.logger = new Logger();
+  }
+
   @Post()
   async create(@Body() user: CreateUserDto): Promise<User>{
-    return await this.userService.create(user);
+    let findDuplicate = await this.userService.findByConditions({ firebase_id: user.firebase_id });
+    if (findDuplicate.length === 0)
+      return await this.userService.create(user);
   }
 
   @Get()
   async findAll(): Promise<User[]>{
     return await this.userService.findAll();
+  }
+
+  @Get('findByFirebaseID/:firebase_id')
+  async findOne(@Param('firebase_id') firebase_id: string) {
+    let results = await this.userService.findByConditions({ firebase_id: firebase_id });
+    if (results.length > 0)
+      return results[0];
   }
 }
